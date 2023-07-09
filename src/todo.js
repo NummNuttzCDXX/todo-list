d538d6318646cc188c0e79ea023c5f99f8bc4e07
NummNuttzCDXX <bbirdy2018@gmail.com>
2023-07-08 23:51:18 -0400

    Remove unneeded object function

--- src/todo.js
+++ src/todo.js
@@ -4,10 +4,5 @@
 export const Todo = (title, desc, due, priority, project) => {
 	let completed = false;
 
-	const setPriority = (newPriority) => {
-		priority = newPriority;
-		return newPriority;
-	}
-
 	return {title, desc, due, priority, setPriority, project, completed}
 }
