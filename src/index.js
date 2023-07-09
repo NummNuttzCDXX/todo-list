66d0ddd600e297d41cd4d30a677ead0e211e421e
NummNuttzCDXX <bbirdy2018@gmail.com>
2023-07-08 23:39:34 -0400

    Add listeners for card edit btn, edit submit btn, and edit form radio btn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 // Main File
 
 import { dom } from "./dom";
+import { Todo } from "./todo"; //TEMP
 
 // Declare array to hold ToDo Obj
-let toDos = [];
+// With Example 
+let exampleTodo = Todo('Feed the cat', 'Food and Water', 'Everyday', 'medium', 'medium'); //TEMP
+let toDos = [ exampleTodo ];
 // Get dropdowns
 let dropdowns = document.querySelectorAll('#sidebar .dropdown');
 
@@ -21,11 +24,7 @@ contentDropdown.forEach(dropdown => dropdown.addEventListener('click', () => dom
 
 // New ToDo Button
 const addBtn = document.querySelector('button.add-new');
-addBtn.addEventListener('click', () => {
-	const formContainer = document.querySelector('#form-container');
-
-	formContainer.style.display = 'flex';
-})
+addBtn.addEventListener('click', () => dom.showForm(document.querySelector('form#new')));
 
 // Close form button
 const closeForm = document.querySelector('img.close-box');
@@ -51,13 +50,16 @@ subBtn.addEventListener('click', () => {
 
 	toDos.push(item.newTodo);
 
+	// Add index to card data attr
+	item.card.setAttribute('data', toDos.length - 1)
+
 	// Toggle Form Off
 	const formContainer = document.querySelector('#form-container');
 	formContainer.style.display = 'none';
 });
 
 // Add/Create Project listener
-const radioBtns = document.querySelectorAll('#form-container input[type="radio"]');
+const radioBtns = document.querySelectorAll('#form-container #new input[type="radio"]');
 radioBtns.forEach(btn => btn.addEventListener('change', () => {
 	const addProj = document.querySelector('label[for="add-proj"]');
 	const createProj = document.querySelector('label[for="create-proj"]');
@@ -71,3 +73,44 @@ radioBtns.forEach(btn => btn.addEventListener('change', () => {
 		createProj.classList.remove('hide');
 	}
 }))
+
+// Edit Todo listener
+let editBtn = document.querySelectorAll('.card .priority img');
+let editCard;
+editBtn.forEach(btn => btn.addEventListener('click', () => {
+	// Toggle card drop
+	dom.toggleCardDrop(btn.parentElement.parentElement)
+
+	// Show edit form/Hide add form
+	dom.showForm(document.querySelector('form#edit'));
+
+	// Save editted card for later use
+	editCard = btn.parentElement.parentElement;
+}))
+
+// Edit Add/Create Project Listener
+const editRadio = document.querySelectorAll('#edit input[type="radio"]');
+editRadio.forEach(btn => btn.addEventListener('change', () => {
+	// Get inputs to show/hide
+	const addProj = document.querySelector('label[for="edit-proj"]');
+	const createProj = document.querySelector('label[for="edit-create-proj"]');
+	const radioAdd = document.querySelector('input#edit-add'); // Get radio button
+
+	if (radioAdd.checked) {
+		addProj.classList.remove('hide');
+		createProj.classList.add('hide');
+	} else {
+		addProj.classList.add('hide');
+		createProj.classList.remove('hide');
+	}
+}))
+
+// Edit Submit Button
+const editSubBtn = document.querySelector('#edit button');
+editSubBtn.addEventListener('click', () => {
+	let editTodo = toDos[editCard.getAttribute('data')];
+	let items = dom.subEdit(editCard, editTodo);
+
+	// hide form
+	document.querySelector('#form-container').style.display = 'none';
+})
